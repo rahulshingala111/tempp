e983230c03c56a98990627102d2da1a10610ba1c
Rahul Shingala <rahulshingala111@gmail.com>
2023-06-28 08:54:28 +0530

    to PC

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "../home/any.txt";
 import { io } from "socket.io-client";
+import { JitsiMeeting } from "@jitsi/react-sdk";
 const socket = io.connect("http://localhost:3001");
 
 function Home() {
   const [data, setData] = useState("");
 
   useEffect(() => {
-    axios
+    axios``
       .get("http://localhost:5000/home")
       .then((response) => {
         setData(response.data.msg);
@@ -23,6 +24,16 @@ function Home() {
     <div>
       <div>Welcome to home</div>
       <div>{data}</div>
+      <div>
+        {/* <JitsiMeeting
+          configOverwrite={{
+            startWithAudioMuted: true,
+            hiddenPremeetingButtons: ["microphone"],
+          }}
+          getIFrameRef={(node) => (node.style.height = "800px")}
+          roomName={"YOUR_CUSTOM_ROOM_NAME"}
+        /> */}
+      </div>
     </div>
   );
 }
